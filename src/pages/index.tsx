ea18ba68cbeba463d97057e3c5972b93b1801c92
Henrique Domanski <henrydomanski@gmail.com>
2021-06-08 12:01:10 -0300

    update 8/6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,25 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ChallengeBox } from "../components/ChallengeBox";
 import { CountdownProvider } from "../contexts/CountdownContext";
+import {GetServerSideProps} from 'next'
+import React from "react";
+import { ChallengesProvider } from "../contexts/ChallengeContext";
 
 
-export default function Home() {
+interface HomeProps {
+      level: number;
+      currentExperience: number;
+      challengesCompleted: number;
+
+}
+
+export default function Home(props: HomeProps) {
   return (
+    <ChallengesProvider 
+    level={props.level} 
+    currentExperience={props.currentExperience}
+    challengesCompleted={props.challengesCompleted}
+    >
     <div className={styles.container}>
       <Head>
         <title>Início | move.it </title>
@@ -35,5 +50,22 @@ export default function Home() {
       </section>
     </CountdownProvider>
   </div>
+  </ChallengesProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  
+
+  const {level, currentExperience, challengesCompleted}  = ctx.req.cookies;
+  
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    }
+  }
+
 }
